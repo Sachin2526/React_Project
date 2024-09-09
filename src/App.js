27ab06cee98cffb8a92c1310c3f8f699acf01179
Sachin25 <dheresachin631@gmail.com>
2024-09-09 10:02:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Resturant from "./component/Basics/Resturant";
+// import Todo from './component/TodoList/Todo';
+import Temp from './component/weather/temp';
+
+ 
+
+const App = () => {
+  // return <Resturant />;
+  // return <Todo />;
+  return <Temp />;
+ 
+};
+
+export default App;
